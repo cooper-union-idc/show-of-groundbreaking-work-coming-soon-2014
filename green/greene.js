3902b198ecba74cb74a3301710091c0f8b908ff7
camila <camila@camilas-MacBook-Pro-2.local>
2014-10-19 20:23:26 -0400

    add slder to main

--- green/greene.js
+++ green/greene.js
@@ -54,22 +54,7 @@ var runAnalysis = function () {
     }
 };
 
- //console.log("efv");
-
 var frameFix = function () {
-
-    var r1 = 150;
-    var r2 = 40;
-
-    var r3 = 255;
-    var r4 = 80;
-
-    var r5 = 70;
-    var r6 = 30;
-
-    console.log(val1, val2);
-     console.log(val3, val4);
-      console.log(val5, val6);
 
     hContext.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
@@ -78,10 +63,7 @@ var frameFix = function () {
         length = data.length,
         i;
 
-
     for (i = 0; i < length; i += 4)
-
-   // if ((data[i] < r1 && data[i] > r2) && (data[i+1] < r3 && data[i+1] > r4) && (data[i+2] < r5 && data[i+2] > r6)) data[i + 3] = 0;
 
     if ((data[i] < val2 && data[i] > val1 ) && (data[i+1] < val4 && data[i+1] > val3) && (data[i+2] < val6 && data[i+2] > val5)) data[i + 3] = 0;
 
